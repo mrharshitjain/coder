26d3576b2b6fd3bdec1cbfaba7a4c138fb951d0a
mrharshitjain <69718629+mrharshitjain@users.noreply.github.com>
2020-08-15 17:42:11 +0530

    Add files via upload

---
+++ py-31.py
@@ -0,0 +1,16 @@
+for i in range(20,30,2):
+    print(i)
+# b
+country = 'INDIA'
+for i in country:
+    print (i)
+# c
+i = 0; sum = 0
+while i < 9:
+   if i % 4 == 0:
+       sum = sum + i
+       i = i + 2
+       print (sum)
+
+
+
